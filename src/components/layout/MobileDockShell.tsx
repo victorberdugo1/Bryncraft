5d8828ebf0ac1025fb2a47a2679355a11a7bdefe
Victor Berdugo <victorberdugo1@gmail.com>
2026-08-04 00:28:53 +0200

    add top butons

--- src/components/layout/MobileDockShell.tsx
+++ src/components/layout/MobileDockShell.tsx
@@ -73,10 +73,10 @@ export function MobileDockShell() {
         icon={Clock}
         open={openEdge.bottom}
         onToggle={() => toggle("bottom")}
-        size="auto"
+        size="min(140px, 20vh)"
         tabPositionClassName="top-0 right-0 rounded-bl-xl flex-row w-14 h-6"
       >
-        <div className="flex flex-col">
+        <div className="flex flex-col pb-4">
           <BottomTimeline />
           <div className="flex justify-end border-t border-border/60 px-3 py-2">
             <ExportPanel
